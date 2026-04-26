c4f31af02b49ed8367bc185bff9f425dc82d7813
alihassanmostafa <alihassan.mostafa@gmail.com>
2026-04-26 12:31:20 +0300

    .

--- command_center_lite/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright 2025 PlantBasedStudio
-# License OPL-1 - See LICENSE file for full copyright and licensing details.
-
-from . import models
-from . import wizard
--- command_center_lite/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright 2025 PlantBasedStudio
-# License OPL-1 - See LICENSE file for full copyright and licensing details.
-
-from . import command_center_item
