90ee712984ea592be8d065e138061c05df910bd6
alihassanmostafa <alihassan.mostafa@gmail.com>
2026-05-02 13:48:38 +0300

    easy language switcher

---
+++ quick_language_switcher/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Quick Language Switcher',
+    'author': 'Odoo Hub',
+    'category': 'Tools',
+    'summary': 'The Quick Language Switcher is an Odoo app that adds a convenient language selector dropdown in the system tray. This allows users to quickly switch between different languages, improving accessibility and user experience within the Odoo platform. quick language changer quick language selector language switcher change language select language switch language odoo language changer odoo language selector quickly language changer for current user quick language selection admin language selection menu language switch button odoo language switcher system tray language switcher odoo quick language toggle language dropdown multilingual odoo interface change language from systray odoo multi language support odoo localization tool user language changer language settings shortcut fast language change odoo language change menu odoo language preference odoo interface language switch language picker switch language from top bar odoo multilingual user support change interface language instant language toggle odoo ui language switcher language change without settings odoo language accessibility global language toggle',
+    'description': """
+        Do you want to quickly switch between languages in Odoo without navigating through settings? With Quick Language Switcher, you can easily select and change the language from the system tray, streamlining your user experience.
+
+        🔹 Key Features: ✅ Quick language switcher directly in the system tray
+        ✅ No more navigating through settings – change languages in just one click
+        ✅ Supports multiple languages for a global user base
+        ✅ Seamless integration with Odoo’s interface
+        ✅ Ideal for businesses with multilingual teams or customers
+        
+        With Quick Language Switcher, enhance your Odoo experience and make language transitions effortless! 🌍🚀
+    """,
+    'maintainer': 'Odoo Hub',
+    'website': 'https://apps.odoo.com/apps/modules/browse?author=Odoo%20Hub',
+    'version': '1.0',
+    'depends': ['web'],
+    'assets': {
+        'web.assets_backend': [
+            'quick_language_switcher/static/src/**/*',
+        ],
+    },
+    'images': ['static/description/banner.png'],
+    'live_test_url': 'https://youtu.be/wP3zitQ3hCw',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
